61c8a852bc1cbebb2544ee9330d9a5a25663ab57
morenod <dmorenocuellar@gmail.com>
2016-04-30 15:56:31 -0400

    added KanyeREST API calls for document search

---
+++ KanyeNETS/src/edu/upenn/nets150/homework5/kanyelyrics/Main.java
@@ -0,0 +1,63 @@
+package edu.upenn.nets150.homework5.kanyelyrics;
+
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		String kanyeREST = "http://www.kanyerest.xyz/api/album/";
+		String myBeautifulDarkTwistedFantasy = "my_beautiful_dark_twisted_fantasy";
+		String watchTheThrone = "watch_the_throne";
+		String yeezus = "yeezus";
+		String lifeOfPablo = "the_life_of_pablo";
+
+		URLManager kanyeMBDTF = new URLManager(kanyeREST + myBeautifulDarkTwistedFantasy);
+		PrintWriter MBDTF = new PrintWriter("my_beautiful_dark_twisted_fantasy.txt", "UTF-8");
+        kanyeMBDTF.sendGet(MBDTF);
+        MBDTF.close();
+        
+        URLManager kanyeWTT = new URLManager(kanyeREST + watchTheThrone);
+		PrintWriter WTT = new PrintWriter("watch_the_throne.txt", "UTF-8");
+        kanyeWTT.sendGet(WTT);
+        WTT.close();
+        
+        URLManager kanyeYZS = new URLManager(kanyeREST + yeezus);
+		PrintWriter YZS = new PrintWriter("yeezus.txt", "UTF-8");
+        kanyeYZS.sendGet(YZS);
+        YZS.close();
+        
+        URLManager kanyeTLOP = new URLManager(kanyeREST + lifeOfPablo);
+		PrintWriter TLOP = new PrintWriter("the_life_of_pablo.txt", "UTF-8");
+        kanyeTLOP.sendGet(TLOP);
+        TLOP.close();
+        
+        Document d1 = new Document("my_beautiful_dark_twisted_fantasy.txt");
+		Document d2 = new Document("watch_the_throne.txt");
+		Document d3 = new Document("yeezus.txt");
+		Document d4 = new Document("the_life_of_pablo.txt");
+
+		
+		ArrayList<Document> documents = new ArrayList<Document>();
+		documents.add(d1);
+		documents.add(d2);
+		documents.add(d3);
+		documents.add(d4);
+
+		Corpus corpus = new Corpus(documents);
+		
+		VectorSpaceModel vectorSpace = new VectorSpaceModel(corpus);
+		
+		
+		for (int i = 0; i < documents.size(); i++) {
+			for (int j = i + 1; j < documents.size(); j++) {
+				Document doc1 = documents.get(i);
+				Document doc2 = documents.get(j);
+				System.out.println("\nComparing " + doc1 + " and " +  doc2);
+				System.out.println(vectorSpace.cosineSimilarity(doc1, doc2));
+			}
+		}
+	}
+
+}
---
+++ KanyeNETS/src/edu/upenn/nets150/homework5/kanyelyrics/URLManager.java
@@ -0,0 +1,117 @@
+package edu.upenn.nets150.homework5.kanyelyrics;
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+/**
+ * This class is used by JSoupQuery and ExtraCredit to establish connections to websites.
+ * Based on the code given to us on Canvas.
+ * @author DanielMoreno
+ *
+ */
+public class URLManager {
+	
+	private URL url;
+    private HttpURLConnection httpConnection;
+    
+    /**
+     * This method establishes the connection based on a String url
+     * @param url
+     */
+    public URLManager(String url){
+    	try{
+            this.url = new URL(url);
+            URLConnection connection = this.url.openConnection();
+            httpConnection = (HttpURLConnection) connection;
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * This method will print the status codes from the connection.
+     */
+    public String printStatusCode(){
+        try {
+            int code = httpConnection.getResponseCode();
+            String message = httpConnection.getResponseMessage();
+
+            return(code + " : " + message);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+		return null;
+    }
+    
+    /**
+     * This method returns the manager's URL in string format
+     * @return
+     */
+    public URL getURL(){
+        return this.url;
+    }
+
+    /**
+     * gets HTML contents of URL
+     * @return String containing HTML elements of page
+     */
+    public String getContents(){
+        StringBuilder contents = new StringBuilder();
+        Scanner in;
+        try{
+            in = new Scanner(httpConnection.getInputStream());
+            while(in.hasNextLine()){
+                String line = in.nextLine();
+                contents.append(line);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return contents.toString();
+    }
+    
+    public void sendGet(PrintWriter writer) throws Exception {
+		// optional default is GET
+		httpConnection.setRequestMethod("GET");
+
+		//add request header
+		//httpConnection.setRequestProperty("User-Agent", USER_AGENT);
+		int responseCode = httpConnection.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+
+		
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(httpConnection.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+		JSONParser parser = new JSONParser();
+		JSONObject albumArray = (JSONObject) parser.parse(response.toString());
+		JSONArray album = (JSONArray) albumArray.get("result");
+		for (int i = 0; i < album.size(); i++) {
+			JSONParser lyricsParser = new JSONParser();
+			JSONObject song = (JSONObject) lyricsParser.parse(album.get(i).toString());
+			writer.println(song.get("lyrics"));
+		}
+		
+
+
+	}
+}
+
